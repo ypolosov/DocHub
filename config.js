bed36ab0e797332d7a7d82321c57e451c5d68afb
RPiontik <r.piontik@mail.ru>
2021-06-07 19:00:25 +0300

    Refactored config

--- config.js
+++ config.js
@@ -1,8 +1,12 @@
-let config = {};
-if (process.env.NODE_ENV === 'production') {
-    config = require('./config/prod').default;
-} else {
-    config = require('./config/dev').default;
-}
-
-export default config;
+export default {
+    "gitlab_server": process.env.VUE_APP_DOCHUB_GITLAB_URL, // Адрес GitLab сервера
+    "root_manifest" : {
+        "project_id": process.env.VUE_APP_DOCHUB_MANIFEST_PROJECT_ID, // Идентификатор проекта в котором размещен корневой манифест
+        "branch": process.env.VUE_APP_DOCHUB_MANOFEST_PROJECT_BRANCH // Ветка в которой размещен корневой манифест
+    },
+    "oauth" : {
+        "APP_ID": process.env.VUE_APP_DOCHUB_APP_ID, // Идентификатор приложения в GitLab
+        "CLIENT_SECRET": process.env.VUE_APP_DOCHUB_CLIENT_SECRET, // Секрет приложения  в GitLab
+        "REQUESTED_SCOPES": "read_repository+api"
+    }
+};
--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,26 @@
 const fs = require('fs');
 
-module.exports = {
+// Дефолтная конфигурация dev-сервера
+let config = {
     devServer: {
         /*
         allowedHosts: [
             'dochub.rabota.space',
             'localhost'
         ],
+        */
+    }
+};
+
+// Подключает сертификаты, если они обнаружены
+if(fs.lstatSync(__dirname + '/certs').isDirectory()) {
+    config.devServer = {
         http2: true,
         https: {
             key: fs.readFileSync(__dirname + '/certs/server.key'),
             cert: fs.readFileSync(__dirname + '/certs/server.cert')
         }
-        */
     }
-};
+}
+
+module.exports = config;
