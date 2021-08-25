db985a3d71f17c48fef19ff29de921849ee12b7c
Romasha <r.piontik@mail.ru>
2021-08-25 16:18:20 +0300

    Добавлена подсветка линков компонентов

--- src/components/Schema/Schema.vue
+++ src/components/Schema/Schema.vue
@@ -19,6 +19,22 @@ export default {
     createSVGElement(tag) {
       return document.createElementNS("http://www.w3.org/2000/svg", tag);
     },
+    getControlsByTarget(target) {
+      const linkID = target.getAttribute('data-link-selector');
+      return document.querySelectorAll(`[data-link-selector="${linkID}"]`);
+    },
+    onOverLink(event) {
+      const controls = this.getControlsByTarget(event.target);
+      for (let i = 0; i < controls.length; i++)
+        controls[i].classList.add("selected");
+    },
+
+    onOutLink(event) {
+      const controls = this.getControlsByTarget(event.target);
+      for (let i = 0; i < controls.length; i++)
+        controls[i].classList.remove("selected");
+    },
+
     postrender (svg) {
       // Строим надписи на связях
       let prefix = 0;
@@ -39,21 +55,32 @@ export default {
         //const selector = `path[id^='${link.linkFrom}'][id$='${link.linkTo}']`
         const linkPath = svg.querySelectorAll(selector)[0];
         if (linkPath) {
+          linkPath.classList.add("link-path");
+          linkPath.style = null;
           const defPathID = `def_${prefix++}_${linkPath.id}`;
-          const path = this.createSVGElement('path');
+          const path = this.createSVGElement('svg:path');
           path.setAttribute('id',defPathID);
           path.setAttribute('d',linkPath.getAttribute('d'));
           defs.appendChild(path);
 
-          const title = this.createSVGElement('text');
+          const title = this.createSVGElement('svg:text');
           title.classList.add('schema-link-title');
           title.setAttribute('text-anchor', 'middle');
           title.setAttribute('dy', -4);
 
-          const titlePath = this.createSVGElement('textPath');
+          const titlePath = this.createSVGElement('svg:textPath');
           titlePath.setAttribute("href", `#${defPathID}`);
           titlePath.setAttribute("startOffset", '50%');
           titlePath.textContent = link.link_title;
+
+          title.setAttribute('data-link-selector', prefix);
+          titlePath.setAttribute('data-link-selector', prefix);
+          titlePath.addEventListener('mouseover', this.onOverLink);
+          titlePath.addEventListener('mouseout', this.onOutLink);
+
+          linkPath.setAttribute('data-link-selector', prefix);
+          linkPath.addEventListener('mouseover', this.onOverLink);
+          linkPath.addEventListener('mouseout', this.onOutLink);
 
           title.appendChild(titlePath);
           svg.appendChild(title);
@@ -108,6 +135,7 @@ export default {
     },
     uml () {
       let uml = `@startuml\n${DSL}\n`;
+      this.orientation === 'horizontal' && (uml += 'left to right direction\n');
       if(this.schema) {
         uml += `title  "${this.makeRef('context', this.schema.id, this.schema.title)}"\n`;
         // Готовим структуру схемы для рендеринга
@@ -129,7 +157,7 @@ export default {
               });
               aspectList.length && (uml += `[\n<b>${title}</b>\n====\n* ${aspectList.join('\n----\n* ')}\n]`);
             } else {
-              uml += `${component.entity} "${title}" as [${component.id}]`;
+              uml += `${component.entity} "${title}" as ${component.id}`;
             }
 
             uml += `\n`
@@ -156,7 +184,15 @@ export default {
     }
   },
   props: {
-    schema: Object
+    schema: Object,
+    orientation: { // Ориентация построения схемы
+      type: String,
+      default: 'horizontal', // По умолчанию - сверху в низ
+      validator: value => [
+          'vertical',
+          'horizontal'
+      ].indexOf(value) >= 0
+    }
   },
   data() {
     return {
@@ -170,6 +206,21 @@ export default {
   .schema-link-title {
     font-size: 12px;
     margin-bottom: 6px;
+  }
+
+  text.selected {
+    font-size: 16px;
+    cursor: pointer;
+  }
+
+  path.link-path {
+    stroke: rgb(52, 149, 219);
+    stroke-width: 1;
+  }
+
+  path.selected {
+    stroke: #F00;
+    stroke-width: 2;
   }
 
 </style>
