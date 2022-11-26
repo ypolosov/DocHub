fdf0cba9a66cf872fe4981f0b76e0698c7aac865
R.Piontik <r.piontik@mail.ru>
2022-11-26 14:33:17 +0300

    Устранена проблема с путями к сущностям в меню. Добавлена возможность казывать внешние URL в меню.

--- src/manifest/query.js
+++ src/manifest/query.js
@@ -245,98 +245,106 @@ const SCHEMA_QUERY = `
 `;
 
 const MENU_QUERY = `
-$append((
-    $GET_TITLE := function($LOCATION) {(
-        $STRUCT := $split($LOCATION, "/");
-        $STRUCT[$count($STRUCT) - 1];
-    )};
+(
+	$isURL := $matcher := /^[a-zA-Z]*\\:.*$/i;
+	$isRoot := $matcher := /^\\/.*$/i;
 
-    $MANIFEST := $;
-    $append([
-            {
-                "title": 'Архитектура',
-                "location": 'Архитектура',
-                "route": 'architect/',
-                "expand": true,
-                "icon": 'home'
-            },
-            {
-                "title": "Контексты",
-                "location": 'Архитектура/Контексты',
-                "icon": 'location_searching'
-            },
-            {
-                "title": "Аспекты",
-                "location": 'Архитектура/Аспекты',
-                "icon": 'visibility',
-                "route": 'aspects/'
-            },
-            {
-                "title": 'Документы',
-                "location": 'Документы',
-                "expand": true,
-                "icon": 'description'
-            },
-            contexts.$spread().{
-                "title": $GET_TITLE($.*.location ? $.*.location : $keys()[0]),
-                "route": 'architect/contexts/' & $keys()[0],
-                "hiden": $.*.location ? false : true,
-                "location": 'Архитектура/Контексты/' & $.*.location,
-                "icon": $.*.icon ? $.*.icon : ''
-            },
-            aspects.$spread().{
-                "title": $GET_TITLE($.*.location),
-                "route": 'architect/aspects/' & $keys()[0],
-                "location": 'Архитектура/Аспекты/' & $.*.location,
-                "icon": $.*.icon ? $.*.icon : ''
-            },
-            docs.$spread().{
-                "title": $GET_TITLE($.*.location),
-                "route": 'docs/' & $keys()[0],
-                "hiden": $.*.location ? false : true,
-                "location": 'Документы/' & $.*.location,
-                "icon": $.*.icon ? $.*.icon : ''
-            },
-            {
-                "title": 'Техрадар',
-                "location": 'Техрадар',
-                "route": 'techradar',
-                "icon": 'track_changes'
-            },
-            technologies.sections.$spread().{
-                "title": $.*.title,
-                "route": 'techradar/' & $keys()[0],
-                "location": 'Техрадар/' & $.*.title
-            },
-            {
-                "title": 'Проблемы',
-                "location": 'Проблемы',
-                "route": 'problems',
-                "icon": 'report_problem'
-            }
-        ][($exists(hiden) and $not(hiden)) or $not($exists(hiden))],
-        entities.*.(
-            $eval(menu, $MANIFEST).{
-                "route": link,
-                "location": location,
-                "icon": icon,
-                "title": $GET_TITLE(location)
-            }
+    $append((
+        $GET_TITLE := function($LOCATION) {(
+            $STRUCT := $split($LOCATION, "/");
+            $STRUCT[$count($STRUCT) - 1];
+        )};
+    
+        $MANIFEST := $;
+        $append([
+                {
+                    "title": 'Архитектура',
+                    "location": 'Архитектура',
+                    "route": 'architect/',
+                    "expand": true,
+                    "icon": 'home'
+                },
+                {
+                    "title": "Контексты",
+                    "location": 'Архитектура/Контексты',
+                    "icon": 'location_searching'
+                },
+                {
+                    "title": "Аспекты",
+                    "location": 'Архитектура/Аспекты',
+                    "icon": 'visibility',
+                    "route": 'aspects/'
+                },
+                {
+                    "title": 'Документы',
+                    "location": 'Документы',
+                    "expand": true,
+                    "icon": 'description'
+                },
+                contexts.$spread().{
+                    "title": $GET_TITLE($.*.location ? $.*.location : $keys()[0]),
+                    "route": 'architect/contexts/' & $keys()[0],
+                    "hiden": $.*.location ? false : true,
+                    "location": 'Архитектура/Контексты/' & $.*.location,
+                    "icon": $.*.icon ? $.*.icon : ''
+                },
+                aspects.$spread().{
+                    "title": $GET_TITLE($.*.location),
+                    "route": 'architect/aspects/' & $keys()[0],
+                    "location": 'Архитектура/Аспекты/' & $.*.location,
+                    "icon": $.*.icon ? $.*.icon : ''
+                },
+                docs.$spread().{
+                    "title": $GET_TITLE($.*.location),
+                    "route": 'docs/' & $keys()[0],
+                    "hiden": $.*.location ? false : true,
+                    "location": 'Документы/' & $.*.location,
+                    "icon": $.*.icon ? $.*.icon : ''
+                },
+                {
+                    "title": 'Техрадар',
+                    "location": 'Техрадар',
+                    "route": 'techradar',
+                    "icon": 'track_changes'
+                },
+                technologies.sections.$spread().{
+                    "title": $.*.title,
+                    "route": 'techradar/' & $keys()[0],
+                    "location": 'Техрадар/' & $.*.title
+                },
+                {
+                    "title": 'Проблемы',
+                    "location": 'Проблемы',
+                    "route": 'problems',
+                    "icon": 'report_problem'
+                }
+            ][($exists(hiden) and $not(hiden)) or $not($exists(hiden))],
+            entities.*.(
+                $eval(menu, $MANIFEST).{
+                    "route": link,
+                    "location": location,
+                    "icon": icon,
+                    "title": $GET_TITLE(location)
+                }
+            )
         )
-    )
-).{
-    "title": "" & title,
-    "route": route ? '/' & route : undefined,
-    "icon": icon,
-    "location": "" & (location ? location : route)
-}^(location), [
-    {
-        "title": 'JSONata',
-        "route": '/devtool',
-        "icon": 'chrome_reader_mode',
-        "location": "devtool"
-    }
-])
+    ).{
+        "title": "" & title,
+        "route": route ? (
+            $isURL(route) ? route
+            : ($isRoot(route) ? route : '/' & route)
+        ) : undefined,
+        "icon": icon,
+        "location": "" & (location ? location : route)
+    }^(location), [
+        {
+            "title": 'JSONata',
+            "route": '/devtool',
+            "icon": 'chrome_reader_mode',
+            "location": "devtool"
+        }
+    ])
+)
 `;
 
 const CONTEXTS_QUERY_FOR_COMPONENT = `
