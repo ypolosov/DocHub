ee3160c264c0a249246ec239f3d14d935bc1fb48
Bejoy <casperyourweb@gmail.com>
2023-05-19 10:38:49 +0300

    added and tested svg loader

--- vue.config.js
+++ vue.config.js
@@ -82,6 +82,9 @@ let config = {
 		hot: process.env.VUE_APP_DOCHUB_HOTRELOAD === 'off' ? false : true
 	},
 	transpileDependencies: ['vueitfy'],
+	chainWebpack: (config) => {
+		config.module.rules.delete("svg");
+	},
 	configureWebpack: {
 		cache: (process.env.VUE_APP_DOCHUB_BUILDING_CACHE || 'memory').toLowerCase() === 'filesystem'
 			? {
@@ -103,6 +106,10 @@ let config = {
 		plugins,
 		module: {
 			rules: [
+				{
+					test: /\.svg$/,
+					loader: 'vue-svg-loader',
+				},
 				{
 					test: /\.mjs$/,
 					include: /node_modules/,
