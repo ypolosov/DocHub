41a157ac7d6a6f6cbeaffe3d6dd413a26e4ded03
R.Piontik <r.piontik@mail.ru>
2023-11-13 11:27:14 +0300

    Добавлена параметризация подключения к Redis

--- src/backend/drivers/redis.mjs
+++ src/backend/drivers/redis.mjs
@@ -8,7 +8,8 @@ let client = null;
 
 export default async function() {
     if (client) return client;
-    client = createClient();
+    const url = process.env.VUE_APP_DOCHUB_REDIS_URL;
+    client = url ? createClient({url}) : createClient();
     client.on('error', err => logger.log(`Error of redis client: ${err.toString()}`, LOG_TAG));
     await client.connect();
     logger.log('Redis client is enabled', LOG_TAG);
